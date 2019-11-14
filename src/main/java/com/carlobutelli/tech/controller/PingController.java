13abb74477fad1ab6084fbbd76453b3be9ca96d2
Carlo Butelli <c.butelli@nxchange.com>
2019-11-14 20:35:35 +0100

    Updated controller with swagger docs

--- src/main/java/com/carlobutelli/tech/controller/PingController.java
+++ src/main/java/com/carlobutelli/tech/controller/PingController.java
@@ -1,5 +1,9 @@
 package com.carlobutelli.tech.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/admin")
+@Api(value="Ping Controller request")
 public class PingController {
 
     private static final Logger log = LoggerFactory.getLogger(PingController.class);
@@ -18,6 +23,12 @@ public class PingController {
         log.info(String.format("%s: %s", transactionId, message));
     }
 
+
+    @ApiOperation(value = "Ping request", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully pong"),
+            @ApiResponse(code = 400, message = "Service broken")
+    })
     @GetMapping(value = "/ping")
     public String ping() {
         String transactionId = UUID.randomUUID().toString();
