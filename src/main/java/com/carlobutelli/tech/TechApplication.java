491812c6bdfdeb3fbb33441df0f7eff4d35faa88
Carlo Butelli <c.butelli@nxchange.com>
2019-11-14 20:35:10 +0100

    Added router for swagger

--- src/main/java/com/carlobutelli/tech/TechApplication.java
+++ src/main/java/com/carlobutelli/tech/TechApplication.java
@@ -2,7 +2,15 @@ package com.carlobutelli.tech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +18,12 @@ public class TechApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return route(GET("/swagger"), req ->
+				ServerResponse.temporaryRedirect(URI.create("swagger-ui.html")).build());
 	}
 
 }
